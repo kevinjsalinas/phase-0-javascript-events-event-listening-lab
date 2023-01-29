a2380ab1e34597a04b5df8b342aea641e3448e09
Kevin Salinas <kevinsalinas155@gmail.com>
2023-01-29 16:33:48 -0500

    completed javascript events listening lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const input = document.getElementById("button");
+    input.addEventListener("click", function() {
+        alert("I was clicked!");
+    });
 }
+
+addingEventListener() 
+
+//hard part is understanding what the test wants you to do... like in the console, it makes sense but making it 
+//work on the test index.js is whole different scenario, I had to carefully look at the what the lesson is asking of me...
+//and work backwards from there -- also, searching google only helps a tiny bit but all the instructions are in the doc.
+
